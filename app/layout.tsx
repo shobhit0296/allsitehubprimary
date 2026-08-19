8dbc09b2b41e2da19041560498e047ce10217fbc
Shobhitvermahere <shobhitvermahere@gmail.com>
2026-08-19 08:17:53 +0530

    update Google Analytics tag to G-3ZSN0JXGJK

--- app/layout.tsx
+++ app/layout.tsx
@@ -149,7 +149,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
         {/* Google Analytics GA4 */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-85SF7NYM1G"
+          src="https://www.googletagmanager.com/gtag/js?id=G-3ZSN0JXGJK"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -158,7 +158,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'G-85SF7NYM1G', {
+            gtag('config', 'G-3ZSN0JXGJK', {
               page_path: window.location.pathname,
             });
           `}
